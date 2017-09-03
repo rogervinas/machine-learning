48885894b3ab68c24a7ca7425fa5a82905e78e96
Roger Viñas Alcon <roger.vinas@scmspain.com>
2017-09-03 21:02:51 +0200

    Ex7: PCA

--- ex7/pca.m
+++ ex7/pca.m
@@ -7,24 +7,17 @@ function [U, S] = pca(X)
 % Useful values
 [m, n] = size(X);
 
-% You need to return the following variables correctly.
-U = zeros(n);
-S = zeros(n);
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should first compute the covariance matrix. Then, you
 %               should use the "svd" function to compute the eigenvectors
-%               and eigenvalues of the covariance matrix. 
+%               and eigenvalues of the covariance matrix.
 %
 % Note: When computing the covariance matrix, remember to divide by m (the
 %       number of examples).
 %
 
-
-
-
-
-
+sigma = (X'*X) / m;
+[ U, S, V ] = svd(sigma);
 
 % =========================================================================
 
